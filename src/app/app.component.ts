707d1a2aa5fb9a182586706194925c9bcc39a422
fayaz <fayaz.khader@clearpeaks.com>
2017-05-20 01:16:36 +0400

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,54 +14,6 @@ export class AppComponent implements OnInit{
   title = 'app works!';
   constructor(){}
   ngOnInit():any {
-
-    var cpplugin = jQuery(".cpgauge");
-     cpplugin.cp_gauge({
-
-        design: {
-            needleColor: "blue",
-            lowerBoxVisible: "false",
-            lowerBoxHeight: 25,
-            lowerBoxBackground: "#AAA",
-            lowerBoxTextcolor: "#FFF",
-            lowerBoxTextSize: 13,
-            upperBoxVisible: "fasle",
-            upperBoxText: "80",
-            upperBoxTextColor: "#777",
-            upperBoxTextSize: 30,
-
-        },
-
-        size: {
-            rangeSize: 25,
-            minorTick: 10,
-        },
-
-        data: {
-            value: '80',
-            dataRange: [{
-                from: 0,
-                to: 60,
-                color: "#9CC96B"
-            }, {
-                from: 60,
-                to: 85,
-                color: "#EFAD4D"
-            }, {
-                from: 85,
-                to: 100,
-                color: "#E85C63"
-            }],
-            textBox: [{
-                    lowerBoxText: "Target is 60"
-                },
-                {
-                lowerBoxText:"Value is 80"
-                },
-
-            ]
-        }
-    });
         
   }
 

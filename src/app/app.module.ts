cf94e37aad76ebb9446ddf2ab9b39d2fe87ba395
fayaz <fayaz.khader@clearpeaks.com>
2017-05-21 20:53:01 +0400

    added comp4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { Comp1Component } from './components/comp1/comp1.component';
 import { Comp2Component } from './components/comp2/comp2.component';
 import { Comp3Component } from './components/comp3/comp3.component';
+import { Comp4Component } from './components/comp4/comp4.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { Comp3Component } from './components/comp3/comp3.component';
     Comp1Component,
     Comp2Component,
     Comp3Component,
+    Comp4Component,
       ],
   imports: [
     BrowserModule,
